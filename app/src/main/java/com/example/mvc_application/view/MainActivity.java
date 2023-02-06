bbd5952fef69d3cf4e549e3c42d8371cb30b865c
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-02-06 09:55:58 +0700

    add View

---
+++ app/src/main/java/com/example/mvc_application/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mvc_application.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mvc_application.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
